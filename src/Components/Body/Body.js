0c505cfc4f9559276561f1c1551ab784a76b57c6
comendrun <kamran.ruhani@gmail.com>
2022-07-11 16:01:02 +0200

    worked on animations for body part of the project

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Body.css";
 import BodyPartOne from "./Body Parts/BodyPartOne";
 import BodyPartThree from "./Body Parts/BodyPartThree";
 import BodyPartTwo from "./Body Parts/BodyPartTwo";
 
+// aos
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 export default function Body({ isDesktop }) {
-  // console.log(isDesktop);
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
   return (
     <section className="body-container flex flex-col gap-y-10 max-w-4xl desktop:gap-y-[10rem] max-w-full">
-      <BodyPartOne desktop={isDesktop} />
-      <BodyPartTwo />
-      <BodyPartThree desktop={isDesktop} />
+
+      <div data-aos="fade-right">
+        <BodyPartOne desktop={isDesktop} />
+      </div>
+
+      <div  data-aos="zoom-out" data-aos-easing=""
+    data-aos-mirror="false"
+    data-aos-once="false">
+        <BodyPartTwo />
+      </div>
+
+      <div data-aos="fade-left">
+        <BodyPartThree desktop={isDesktop} />
+      </div>
     </section>
   );
 }
